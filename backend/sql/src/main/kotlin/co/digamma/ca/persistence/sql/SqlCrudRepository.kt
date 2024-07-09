41e70c49f8376ad5cba62c12084e41ad709c068a
Ahmad Shahwan <ahmad.shahwan@gmail.com>
2024-07-09 22:35:44 +0200

    Generalize retrieve and delete in SQL

--- backend/sql/src/main/kotlin/co/digamma/ca/persistence/sql/SqlCrudRepository.kt
+++ backend/sql/src/main/kotlin/co/digamma/ca/persistence/sql/SqlCrudRepository.kt
@@ -1,7 +1,49 @@
 package co.digamma.ca.persistence.sql
 
+import co.digamma.ca.domain.api.Page
+import co.digamma.ca.domain.api.PageSpecs
 import co.digamma.ca.domain.api.model.Model
 import co.digamma.ca.domain.spi.CrudRepository
+import co.digamma.ca.persistence.jooq.media.tables.references.IMAGE
+import org.jooq.Record
+import org.jooq.Table
+import org.jooq.TableField
+import kotlin.reflect.KClass
 
-interface SqlCrudRepository<T: Model>: CrudRepository<T> {
+abstract class SqlCrudRepository<T: Model, R: Record>(
+    protected val table: Table<R>,
+    protected val idField: TableField<R, String?>,
+    protected val dsl: org.jooq.DSLContext,
+    protected val modelType: KClass<T>
+) : CrudRepository<T> {
+
+    override fun retrieve(id: String): T? {
+        return dsl.selectFrom(this.table)
+            .where(this.idField.eq(id))
+            .fetchOne(this::toModel)
+    }
+
+    override fun retrieve(pageSpecs: PageSpecs): Page<T> {
+        val total = dsl.selectCount().from(IMAGE).fetchOne(0, Int::class.java)!!
+        val list =  dsl.selectFrom(this.table)
+            .offset(pageSpecs.index * pageSpecs.size)
+            .limit(pageSpecs.size)
+            .fetch(this::toModel)
+        return Page(list, pageSpecs.index, pageSpecs.size, total)
+    }
+
+    override fun retrieveAll(): List<T> {
+        return dsl.selectFrom(this.table)
+            .fetch(this::toModel)
+    }
+
+    override fun delete(id: String) {
+        dsl.deleteFrom(this.table)
+            .where(this.idField.eq(id))
+            .execute()
+    }
+
+    protected fun toModel(record: R): T {
+        return record.into(this.modelType.java)
+    }
 }
--- backend/sql/src/main/kotlin/co/digamma/ca/persistence/sql/media/SqlImageRepository.kt
+++ backend/sql/src/main/kotlin/co/digamma/ca/persistence/sql/media/SqlImageRepository.kt
@@ -1,32 +1,15 @@
 package co.digamma.ca.persistence.sql.media
 
-import co.digamma.ca.domain.api.Page
-import co.digamma.ca.domain.api.PageSpecs
 import co.digamma.ca.domain.api.media.Image
 import co.digamma.ca.domain.spi.media.ImageRepository
+import co.digamma.ca.persistence.jooq.media.tables.records.ImageRecord
 import co.digamma.ca.persistence.jooq.media.tables.references.IMAGE
+import co.digamma.ca.persistence.sql.SqlCrudRepository
 import org.jooq.DSLContext
 
-class SqlImageRepository(
-    private val dsl: DSLContext
-): ImageRepository {
-
-    override fun retrieve(id: String): Image? {
-        return dsl.selectFrom(IMAGE).where(IMAGE.ID.eq(id)).fetchOneInto(Image::class.java)
-    }
-
-    override fun retrieve(pageSpecs: PageSpecs): Page<Image> {
-        val total = dsl.selectCount().from(IMAGE).fetchOne(0, Int::class.java)!!
-        val list =  dsl.selectFrom(IMAGE)
-            .offset(pageSpecs.index * pageSpecs.size)
-            .limit(pageSpecs.size)
-            .fetchInto(Image::class.java)
-        return Page(list, pageSpecs.index, pageSpecs.size, total)
-    }
-
-    override fun retrieveAll(): List<Image> {
-        return dsl.selectFrom(IMAGE).fetchInto(Image::class.java)
-    }
+class SqlImageRepository(dsl: DSLContext):
+    SqlCrudRepository<Image, ImageRecord>(IMAGE, IMAGE.ID, dsl, Image::class),
+    ImageRepository {
 
     override fun create(model: Image): Image {
         dsl.insertInto(IMAGE)
@@ -46,9 +29,5 @@ class SqlImageRepository(
             .where(IMAGE.ID.eq(model.id))
             .execute()
         return model
-    }
-
-    override fun delete(id: String) {
-        dsl.deleteFrom(IMAGE).where(IMAGE.ID.eq(id)).execute()
     }
 }
